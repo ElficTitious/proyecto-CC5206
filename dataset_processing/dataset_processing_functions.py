17858a32eb1b5b280bc3175068be225f58918dca
Ismael Correa <ismael.correa.25@gmail.com>
2021-06-02 23:47:16 -0400

    wrote script for generating grouped by datasets

--- dataset_processing/dataset_processing_functions.py
+++ dataset_processing/dataset_processing_functions.py
@@ -64,5 +64,54 @@ def cleanse_dataset(dataset_name):
 
     # Finally we write the cleansed dataset.
     path_file_to_write = os.path.join(this_file_path, "datasets/cleansed", dataset_name)
-    data_frame.to_csv(path_file_to_write)
+    data_frame.to_csv(path_file_to_write, index=False, encoding = "ISO-8859-1")
 
+
+def generate_grouped_dataset(dataset_name, year):
+    """Method used for generating the grouped datasets used in this project.
+    Given the name of the cleansed dataset in the directory /../datasets/cleansed, and 
+    the year that datasets corresponds to, it generates generates a number of grouped
+    datasets aggregated by mean for PROM_GRAL and ASISTENCIA, and writes them in
+    /../datasets/datasets_grouped/<year>.
+
+    Parameters
+    ----------
+    dataset_name : [str] Name of the .csv file, in /../datasets/cleansed, that contains the
+                   dataset for which to generate the new grouped datasets.
+    year : [int] Year that dataset_name corresponds to.
+    """
+    
+    # First we generate the path of the dataset.
+    this_file_path = os.path.dirname(os.path.realpath('__file__'))
+    path_file_to_clean = os.path.join(this_file_path, "datasets/cleansed", dataset_name)
+
+    # Now we read the dataset.
+    data_frame = pd.read_csv(path_file_to_clean, encoding = "ISO-8859-1")
+   
+    # We generate the new datasets. 
+    means_depe_by_comuna = data_frame.groupby(["AGNO", "COD_REG_RBD", "COD_COM_RBD", "COD_DEPE"], as_index=False) \
+                           [["PROM_GRAL", "ASISTENCIA"]].agg("mean").round(1)
+    means_depe_by_region = data_frame.groupby(["AGNO", "COD_REG_RBD", "COD_DEPE"], as_index=False) \
+                           [["PROM_GRAL", "ASISTENCIA"]].agg("mean").round(1)
+    means_depe = data_frame.groupby(["AGNO", "COD_DEPE"], as_index=False)[["PROM_GRAL", "ASISTENCIA"]].agg("mean")
+    means_comuna_by_region = data_frame.groupby(["AGNO", "COD_REG_RBD", "COD_COM_RBD"], as_index=False) \
+                             [["PROM_GRAL", "ASISTENCIA"]].agg("mean").round(1)
+    means_region = data_frame.groupby(["AGNO", "COD_REG_RBD"], as_index=False)[["PROM_GRAL", "ASISTENCIA"]].agg("mean").round(1)
+    
+    # Dictionary for writing files.
+    grouped_datasets_dict = {"means_depe_by_comuna" : means_depe_by_comuna, 
+                             "means_depe_by_region" : means_depe_by_region, 
+                             "means_depe" : means_depe, "means_comuna_by_region" : means_comuna_by_region, 
+                             "means_region" : means_region}
+
+    # We write all the datasets for the given year
+    for name in grouped_datasets_dict.keys():
+        file_name = name + "_{}.csv".format(year)
+        print("Writing ", file_name)
+        path_file_to_write = os.path.join(this_file_path,
+                                          "datasets/datasets_grouped/{}".format(year), file_name)
+        
+        grouped_datasets_dict[name].to_csv(path_file_to_write, index=False, encoding = "ISO-8859-1")
+        
+       
+     
